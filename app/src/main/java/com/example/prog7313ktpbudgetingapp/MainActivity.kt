280528a978b4d22b111a1fc084e173a627e62937
ST10401917 <st10401917@vcconnect.edu.za>
2026-05-23 22:20:11 +0200

    Connected to firebase for login

--- app/src/main/java/com/example/prog7313ktpbudgetingapp/MainActivity.kt
+++ app/src/main/java/com/example/prog7313ktpbudgetingapp/MainActivity.kt
@@ -3,31 +3,50 @@ package com.example.prog7313ktpbudgetingapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 import kotlin.jvm.java
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var loginButton: Button
-    private lateinit var registerButton: Button
+    private lateinit var username: EditText
+    private lateinit var password: EditText
+    private lateinit var loginbutton: Button
+    private lateinit var registerbutton: Button
+
+    private lateinit var auth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        loginButton = findViewById(R.id.loginbutton)
-        registerButton = findViewById(R.id.registerbutton)
+        username = findViewById(R.id.username)
+        password = findViewById(R.id.password)
+        loginbutton = findViewById(R.id.loginbutton)
+        registerbutton = findViewById(R.id.registerbutton)
 
-        loginButton.setOnClickListener {
-            // Handle login button click
+        auth = FirebaseAuth.getInstance()
+
+        loginbutton.setOnClickListener {
+            val usernameText = username.text.toString()
+            val passwordText = password.text.toString()
+
+            if (usernameText.isEmpty() || passwordText.isEmpty()) {
+                Toast.makeText(this, "Please fill in all field", Toast.LENGTH_SHORT).show()
+            } else{
+                loginUser(usernameText, passwordText)
+            }
         }
 
-        registerButton.setOnClickListener {
-            //startActivity(Intent(this, RegisterActivity::class.java))
+        registerbutton.setOnClickListener {
+            startActivity(Intent(this, RegisterPage::class.java))
             finish()
         }
 
@@ -37,4 +56,17 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+    private fun loginUser(usernameText: String, passwordText: String) {
+        auth.signInWithEmailAndPassword(usernameText, passwordText)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, HomePage::class.java))
+                    finish()
+                } else {
+                    Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
 }
--- app/src/main/java/com/example/prog7313ktpbudgetingapp/RegisterPage.kt
+++ app/src/main/java/com/example/prog7313ktpbudgetingapp/RegisterPage.kt
@@ -1,20 +1,76 @@
 package com.example.prog7313ktpbudgetingapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterPage : AppCompatActivity() {
+
+    private lateinit var username: EditText
+    private lateinit var password: EditText
+    private lateinit var confirmPassword: EditText
+    private lateinit var registerButton: Button
+    private lateinit var toLoginButton: Button
+    private lateinit var auth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register_page)
+
+        username = findViewById(R.id.username)
+        password = findViewById(R.id.password)
+        confirmPassword = findViewById(R.id.confirmPassword)
+        registerButton = findViewById(R.id.registerButton)
+        toLoginButton = findViewById(R.id.toLoginButton)
+
+        auth = FirebaseAuth.getInstance()
+
+        registerButton.setOnClickListener {
+            val usernameText = username.text.toString()
+            val passwordText = password.text.toString()
+            val confirmPasswordText = confirmPassword.text.toString()
+
+            if (TextUtils.isEmpty(usernameText) || TextUtils.isEmpty(passwordText) || TextUtils.isEmpty(confirmPasswordText)) {
+                Toast.makeText(this, "Empty Credentials", Toast.LENGTH_SHORT).show()
+            } else if (passwordText != confirmPasswordText) {
+                Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
+            } else {
+                registerUser(usernameText, passwordText)
+            }
+        }
+
+        toLoginButton.setOnClickListener {
+            finish()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun registerUser(usernameText: String, passwordText: String) {
+        auth.createUserWithEmailAndPassword(usernameText, passwordText)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                } else {
+                    Toast.makeText(this, "Registration Failed: ${task.exception?.message}",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
